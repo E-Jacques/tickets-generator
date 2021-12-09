a9c0cf2079e2c44912d1dd51a8b5f9c6ef73e87a
Emilien <emil00j77@gmail.com>
2021-12-09 11:16:23 +0100

    tea

--- server.js
+++ server.js
@@ -10,43 +10,43 @@ loadData("./hash.txt");
 app.get("/:hash", getHashAviability);
 app.post("/:hash", useHash);
 function loadData(hashPath) {
-    var data = (0, fs_1.readFileSync)(hashPath, { encoding: "ascii" });
-    var lines = data.split("\n");
-    for (var _i = 0, lines_1 = lines; _i < lines_1.length; _i++) {
-        var line = lines_1[_i];
-        if (line) {
-            allHashList.push(line);
-        }
+  var data = (0, fs_1.readFileSync)(hashPath, { encoding: "ascii" });
+  var lines = data.split("\n");
+  for (var _i = 0, lines_1 = lines; _i < lines_1.length; _i++) {
+    var line = lines_1[_i];
+    if (line) {
+      allHashList.push(line);
     }
+  }
 }
 function useHash(req, res) {
-    var hash = req.params.hash;
-    if (allHashList.includes(hash) && !usedHashList.includes(hash)) {
-        usedHashList.push(hash);
-    }
-    return res.render("index", {
-        hash: hash,
-        avaibility: "Déjà utilisé ...",
-        title: "Marché de Noel !"
-    });
+  var hash = req.params.hash;
+  if (allHashList.includes(hash) && !usedHashList.includes(hash)) {
+    usedHashList.push(hash);
+  }
+  return res.render("index", {
+    hash: hash,
+    avaibility: "Déjà utilisé ...",
+    title: "Marché de Noel !",
+  });
 }
 function getHashAviability(req, res) {
-    var hash = req.params.hash;
-    var o = {
-        hash: hash,
-        avaibility: "Utilisable !",
-        title: "Marché de Noel !"
-    };
-    if (!allHashList.includes(hash)) {
+  var hash = req.params.hash;
+  var o = {
+    hash: hash,
+    avaibility: "Utilisable !",
+    title: "Marché de Noel !",
+  };
+  if (!allHashList.includes(hash)) {
     o.avaibility = "Inconnu :(";
     return res.render("index", o);
-    }
-    if (allHashList.includes(hash) && !usedHashList.includes(hash)) {
-        return res.render("index", o);
-    }
-    o.avaibility = "Déjà utilisé ...";
+  }
+  if (allHashList.includes(hash) && !usedHashList.includes(hash)) {
     return res.render("index", o);
+  }
+  o.avaibility = "Déjà utilisé ...";
+  return res.render("index", o);
 }
 app.listen(3001, function () {
-    console.log("Server online on port 3000");
+  console.log("Server online on port 3000");
 });
